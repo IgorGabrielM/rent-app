bee783e962ae41c94752a7393259b993e4bf4d85
igorGabrielM <igorgabprofissional@gmail.com>
2023-01-12 17:02:37 -0300

    ionic function

--- rent-app-client/capacitor.config.ts
+++ rent-app-client/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'rent-app',
+  appName: 'rent-app-client',
   webDir: 'www',
   bundledWebRuntime: false
 };
