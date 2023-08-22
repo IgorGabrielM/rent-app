c847362e9d75a063f908f8aa023ae563e8e319a1
igorGabrielM <igorgabprofissional@gmail.com>
2023-08-21 21:42:54 -0300

    fix: implement acordion of assets and values contract

--- rent-app-client/src/app/components/contract-card/contract-card.component.ts
+++ rent-app-client/src/app/components/contract-card/contract-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ContractModel } from 'src/@core/models/contract.model';
 import { ModalContractPdfComponent } from '../modal-contract-pdf/modal-contract-pdf.component';
+import { AssetModel } from 'src/@core/models/asset.model';
 
 @Component({
   selector: 'contract-card',
@@ -30,5 +31,20 @@ export class ContractCardComponent implements OnInit {
     });
     return await modal.present();
   }
+
+  getValueOfQuantity(asset: AssetModel) {
+    return Number(asset.assetCategory.value) * asset.quantity
+  }
+
+  getTotalValue(): number {
+    let totalValue: number = 0;
+
+    this.contract.assets.forEach((asset) => {
+      totalValue += Number(asset.assetCategory.value) * asset.quantity;
+    });
+
+    return totalValue;
+  }
+
 
 }
